f0fcced54e91e22a7d30c1c9e795a1a0987b2df7
Putri Dinar <79742186+putridinar@users.noreply.github.com>
2025-01-19 23:20:06 +0700

    Update

---
+++ assets/js/function.js
@@ -0,0 +1,13 @@
+---
+---
+
+<script>
+function onLoadSkeleton() {
+  const skeletonEl = document.getElementById('skeleton');
+  const dataEl = document.getElementById('myBlog');
+  setTimeout(() => {
+    skeletonEl.style.display = 'none';
+    dataEl.style.display = 'grid';
+  }, 2500);
+}
+</script>
