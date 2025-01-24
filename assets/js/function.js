dc0ad80108c00382dc0ca00061e151d639b5c081
Putri Dinar <79742186+putridinar@users.noreply.github.com>
2025-01-24 14:20:01 +0700

    Update function

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,3 +1,5 @@
+# skeleton #
+
 function onLoadSkeleton() {
   const skeletonEl = document.getElementById('skeleton');
   const dataEl = document.getElementById('myBlog');
@@ -6,3 +8,35 @@ function onLoadSkeleton() {
     dataEl.style.display = 'grid';
   }, 2500);
 }
+
+# lazy #
+
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyloadImages = document.querySelectorAll("img.lazy");    
+  var lazyloadThrottleTimeout;
+  
+  function lazyload () {
+    if(lazyloadThrottleTimeout) {
+      clearTimeout(lazyloadThrottleTimeout);
+    }    
+    
+    lazyloadThrottleTimeout = setTimeout(function() {
+        var scrollTop = window.pageYOffset;
+        lazyloadImages.forEach(function(img) {
+            if(img.offsetTop < (window.innerHeight + scrollTop)) {
+              img.src = img.dataset.src;
+              img.classList.remove('lazy');
+            }
+        });
+        if(lazyloadImages.length == 0) { 
+          document.removeEventListener("scroll", lazyload);
+          window.removeEventListener("resize", lazyload);
+          window.removeEventListener("orientationChange", lazyload);
+        }
+    }, 20);
+  }
+  
+  document.addEventListener("scroll", lazyload);
+  window.addEventListener("resize", lazyload);
+  window.addEventListener("orientationChange", lazyload);
+});
