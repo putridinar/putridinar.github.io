e93190838e47623c917c153b96bfa25d229271ab
Putri Dinar <79742186+putridinar@users.noreply.github.com>
2025-02-21 09:02:49 +0700

    Update function

--- assets/js/function.js
+++ assets/js/function.js
@@ -6,3 +6,15 @@ function onLoadSkeleton() {
     dataEl.style.display = 'grid';
   }, 2500);
 }
+
+{$ if page.url='/admin/login/' $}
+// Add the fingerprint login logic here
+document.getElementById('fingerprintLoginButton').addEventListener('click', function() {
+    const fpPromise = FingerprintJS.load();
+    fpPromise.then(fp => fp.get()).then(result => {
+        const visitorId = result.visitorId;
+        // Handle the fingerprint login logic here
+        console.log(visitorId);
+    });
+});
+{$ endif $}
